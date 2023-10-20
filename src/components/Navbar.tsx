a6219a4809987c1983d70680887b62d79061a5c5
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2023-10-21 03:50:10 +0530

    Navbar Login State

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,19 @@
 import Link from "next/link";
-import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/server";
+import {
+  LoginLink,
+  LogoutLink,
+  RegisterLink,
+  getKindeServerSession,
+} from "@kinde-oss/kinde-auth-nextjs/server";
 
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import { buttonVariants } from "./ui/button";
 import { ArrowRight } from "lucide-react";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const { getUser } = getKindeServerSession();
+  const user = getUser();
+
   return (
     <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
@@ -27,21 +35,32 @@ export default function Navbar() {
                 Pricing
               </Link>
 
-              <LoginLink
-                className={buttonVariants({
-                  variant: "ghost",
-                  size: "sm",
-                })}>
-                Sign In
-              </LoginLink>
+              {!user ? (
+                <>
+                  <LoginLink
+                    className={buttonVariants({
+                      variant: "ghost",
+                      size: "sm",
+                    })}>
+                    Sign In
+                  </LoginLink>
 
-              <RegisterLink
-                className={buttonVariants({
-                  size: "sm",
-                })}>
-                Get Started
-                <ArrowRight className="ml-1.5 h-5 w-5" />
-              </RegisterLink>
+                  <RegisterLink
+                    className={buttonVariants({
+                      size: "sm",
+                    })}>
+                    Get Started
+                    <ArrowRight className="ml-1.5 h-5 w-5" />
+                  </RegisterLink>
+                </>
+              ) : (
+                <LogoutLink
+                  className={buttonVariants({
+                    size: "sm",
+                  })}>
+                  Log out
+                </LogoutLink>
+              )}
             </>
           </div>
         </div>

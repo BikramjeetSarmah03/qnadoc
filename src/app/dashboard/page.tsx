924481630f19c47b573dd68aa3bf1a8a2f7b158a
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2023-10-21 03:47:14 +0530

    Auth + Prisma

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,31 @@
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
+import {
+  LogoutLink,
+  getKindeServerSession,
+} from "@kinde-oss/kinde-auth-nextjs/server";
 
-export default function Dashboard() {
+import { buttonVariants } from "@/components/ui/button";
+import { db } from "../db";
+
+export default async function Dashboard() {
   const { getUser } = getKindeServerSession();
-
   const user = getUser();
 
-  if (!user || !user.id) return redirect("/auth-callback?origin=dashboard");
+  if (!user || !user.id) redirect("/auth-callback?origin=dashboard");
 
-  return <div>{user.email}</div>;
+  const dbUser = await db.user.findFirst({
+    where: {
+      id: user.id,
+    },
+  });
+
+  if (!dbUser) redirect("/auth-callback?origin=dashboard");
+
+  return (
+    <div>
+      {user.email}
+
+      <LogoutLink className={buttonVariants()}>Log out</LogoutLink>
+    </div>
+  );
 }
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,17 +1,33 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { publicProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
+import { db } from "@/app/db";
 
 export const appRouter = router({
-  authCallback: publicProcedure.query(() => {
+  authCallback: publicProcedure.query(async () => {
     const { getUser } = getKindeServerSession();
     const user = getUser();
 
     if (!user.id || !user.email) throw new TRPCError({ code: "UNAUTHORIZED" });
 
     // check if user is in the database
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: user.id,
+      },
+    });
 
-    return { success: false };
+    if (!dbUser) {
+      // create user in db
+      await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email,
+        },
+      });
+    }
+
+    return { success: true };
   }),
 });
 
